5c1643f5aebf41718da95d68564bc5e135632cc4
Spencer <git@spencer.moe>
2026-02-04 10:52:48 +0000

    UI: Separate app and restore dialog logic.

--- src/ui.rs
+++ src/ui.rs
@@ -1,8 +1,9 @@
-// SPDX-FileCopyrightText: 2025 Spencer
+// SPDX-FileCopyrightText: 2025-2026 Spencer
 // SPDX-License-Identifier: AGPL-3.0-only
 
 mod app;
 mod handlers;
+mod restore_dialog;
 
 pub use app::run;
-pub use app::run_restore_dialog;
+pub use restore_dialog::run_restore_dialog;
--- src/ui/app.rs
+++ src/ui/app.rs
@@ -4,12 +4,8 @@
 slint::include_modules!();
 
 use super::handlers::{games, settings};
-use crate::archive::{ArchiveReader, Error as ArchiveError};
 use crate::config::Config as AletheiaConfig;
-use crate::gamedb;
-use crate::operations::{RestoreError, restore_game};
 use std::cell::RefCell;
-use std::path::Path;
 use std::process::Command;
 use std::rc::Rc;
 
@@ -63,63 +59,6 @@ pub fn run(config: &AletheiaConfig) {
     settings::setup(&app_weak, &cfg);
 
     app.run().unwrap();
-}
-
-pub fn run_restore_dialog(config: &AletheiaConfig, archive_path: &str) {
-    let archive_path = Path::new(archive_path);
-    if !archive_path.exists() {
-        return;
-    }
-
-    let Ok(reader) = ArchiveReader::open(archive_path) else {
-        return;
-    };
-
-    let cfg = Rc::new(RefCell::new(config.clone()));
-    let restore_dialog = RestoreDialog::new().unwrap();
-    let restore_logic = restore_dialog.global::<RestoreLogic>();
-
-    restore_logic.on_cancel({
-        let restore_weak = restore_dialog.as_weak().unwrap();
-
-        move || {
-            restore_weak.hide().unwrap();
-        }
-    });
-
-    restore_logic.on_restore({
-        let restore_weak = restore_dialog.as_weak().unwrap();
-
-        move || {
-            let restore_logic = restore_weak.global::<RestoreLogic>();
-            let installed_games = gamedb::get_installed_games();
-            let game_name = restore_logic.get_game_name();
-            let game_name = game_name.as_str();
-
-            let Some(game) = installed_games.iter().find(|g| g.name == game_name) else {
-                restore_logic.set_error("GAME_NOT_INSTALLED".into());
-                return;
-            };
-
-            if let Err(RestoreError::Archive(e)) = restore_game(game, &cfg.borrow()) {
-                let error_message = match e {
-                    ArchiveError::ChecksumMismatch(..) | ArchiveError::FileNotFound(_) => "ARCHIVE_CORRUPTED",
-                    ArchiveError::InvalidArchive | ArchiveError::Serialization(_) => "INVALID_ARCHIVE",
-                    ArchiveError::Io(_) => "IO_ERROR",
-                    ArchiveError::UnsupportedVersion(_) => "UNSUPPORTED_ARCHIVE_VERSION"
-                };
-
-                restore_logic.set_error(error_message.into());
-            } else {
-                restore_weak.hide().unwrap();
-            }
-        }
-    });
-
-    restore_logic.set_game_name(reader.game.into());
-    slint::set_xdg_app_id("moe.spencer.Aletheia").unwrap();
-
-    restore_dialog.run().unwrap();
 }
 
 fn setup_app_handlers(app: &App) {
